bfa4929364d3b691835155fa27a9bd5d35b21615
julian3493 <julianramosarevalo@gmail.com>
2021-05-11 16:32:57 -0500

    add assets for game scene

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -5,7 +5,18 @@ import logo from '../assets/logo.png';
 import box from '../assets/grey_box.png';
 import blueBox from '../assets/blue_boxCheckmark.png';
 import music from '../assets/TownTheme.mp3';
-
+import sprBg0 from '../content/sprBg0.png';
+import sprBg1 from '../content/sprBg1.png';
+import sprExplosion from '../content/sprExplosion.png';
+import sprEnemy0 from '../content/sprEnemy0.png';
+import sprEnemy1 from '../content/sprEnemy1.png';
+import sprEnemy2 from '../content/sprEnemy2.png';
+import sprLaserEnemy0 from '../content/sprLaserEnemy0.png';
+import sprLaserPlayer from '../content/sprLaserPlayer.png';
+import sprPlayer from '../content/sprPlayer.png';
+import sndExplode0 from '../content/sndExplode0.wav';
+import sndExplode1 from '../content/sndExplode1.wav';
+import sndLaser from '../content/sndLaser.wav';
 
 export default class PreloaderScene extends Phaser.Scene {
   constructor () {
@@ -91,6 +102,32 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image('box', box);
     this.load.image('checkedBox', blueBox);
     this.load.audio('bgMusic', music);
+
+    // load game scene assets
+    this.load.image("sprBg0", sprBg0);
+    this.load.image("sprBg1", sprBg1);
+    this.load.spritesheet("sprExplosion", sprExplosion, {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    this.load.spritesheet("sprEnemy0", sprEnemy0, {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.image("sprEnemy1", sprEnemy1);
+    this.load.spritesheet("sprEnemy2", sprEnemy2, {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.image("sprLaserEnemy0", sprLaserEnemy0);
+    this.load.image("sprLaserPlayer", sprLaserPlayer);
+    this.load.spritesheet("sprPlayer", sprPlayer, {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.audio("sndExplode0", sndExplode0);
+    this.load.audio("sndExplode1", sndExplode1);
+    this.load.audio("sndLaser", sndLaser);
   }
  
   create () {
